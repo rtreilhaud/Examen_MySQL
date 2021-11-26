d9cd84a44b36a80435ff4ee2b05ecf7375d0a770
Rémi Treilhaud <remi.treilhaud@vingineers.com>
2021-11-26 14:12:33 +0100

    Fix JOIN

--- TP_02/ex05.sql
+++ TP_02/ex05.sql
@@ -1,15 +1,9 @@
 -- Sélectionnez les trajets de tous les pilotes.
 
--- Tous les pilotes
-SELECT p.`name`, pt.`trip_id` FROM `pilots` AS p
-LEFT JOIN  `pilot_trip` AS pt
-ON p.`certificate` = pt.`certificate`;
-
--- Seulement les pilotes avec des trajets
-SELECT p.`name`, pt.`trip_id`, t.`departure`, t.`arrival` FROM `pilots` AS p
+SELECT p.`name`, t.`name`, t.`departure`, t.`arrival` FROM `pilots` AS p
 LEFT JOIN  `pilot_trip` AS pt
 ON p.`certificate` = pt.`certificate`
-INNER JOIN `trips` AS t
+LEFT JOIN `trips` AS t
 ON t.`id` = pt.`trip_id`;
 
 
